8ceb4aa7ec17f1e1a184aa2c7296298112a606cf
HigherEd <37836599+hlawrence-esrica@users.noreply.github.com>
2018-10-18 13:40:56 -0400

    d

--- assets/js/app_getdata.js
+++ assets/js/app_getdata.js
@@ -46,10 +46,11 @@ var rinks_layer = L.esri.featureLayer({
             reading_date = new Date(v.properties.CreationDate);
             readings.push(parseInt(v.properties.reading_conditions)); // put all data for the chart in the popup box
 
+
+            console.log(v);
             //layer.Creator = v.properties.Creator;
             markersObject[v.properties.Creator] = {new L.marker(v.geometry.latlng).addTo(map), v.properties.Creator};
 
-            console.log(v);
             console.log(markers[Object]);
 
             if(counter >= feature_count){ // this will activate for the last record in the list of readings ordered by date ASC
