30ad2b8522a7c003baefead8d56fef64178f1f3e
HigherEd <37836599+hlawrence-esrica@users.noreply.github.com>
2018-10-19 07:32:29 -0400

    e

--- assets/js/app_getdata.js
+++ assets/js/app_getdata.js
@@ -35,7 +35,7 @@ var rinks_layer = L.esri.featureLayer({
       var coords = new L.LatLng(feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
       var rink_name_data = feature.properties.rink_name;
 
-      var reading_date = {}, reading_skateable = {}, reading_conditions = {};
+      var reading_date = [], reading_skateable = [], reading_conditions = [];
 
       L.esri.query({
         url: readings_url,
