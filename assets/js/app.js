98b59dfe1c2f6a2653979a7366deeb4a46bbcb01
HigherEd <37836599+hlawrence-esrica@users.noreply.github.com>
2018-10-23 12:11:09 -0400

    d

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,13 +10,14 @@ var skateableLayer = new L.featureGroup();
 var notskateableLayer = new L.featureGroup();
 
 // links to the two Survey123 hosted feature services
-var rinks_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/survey123_47bbdd102ad44affb7a5835f9fb4085e/FeatureServer/0';
-var readings_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/survey123_c3d35e73bb6e47fbb0b6d17f687a954e/FeatureServer/0';
+var rinks_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/service_29b1d3930876401ab63286e490c601f6/FeatureServer/0';
+var readings_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/service_f8d1476ec5df4d349f953bd14a880be8/FeatureServer/0';
+var add_readings_form = "https://arcg.is/0aruLi";
 var show_skateable_how_many_days_ago = 7;
 
 
 $("#enter_rink_data").click(function() {
-window.open("https://arcg.is/1uCjaS");
+window.open(add_readings_form);
 });
 
 getData();
--- assets/js/app_sidebar.js
+++ assets/js/app_sidebar.js
@@ -88,29 +88,29 @@ function sidebarClick(id) {
   }
 }
 
-function syncSidebar() {
-   Empty sidebar features 
-  $("#feature-list tbody").empty();
-  /* Loop through rinks layer and add only features which are in the map bounds */
-    if (map.hasLayer(rinksLayer)) {
-        var Q = L.esri.query({url: rinks_url}); //change to readings_url once it is working 
-        Q.within(map.getBounds());
-        Q.run(function(error, featureCollection){
-          //alert(featureCollection);
-          if(featureCollection) {  
-            for (i = 0; i < featureCollection.features.length; i++) { 
-              //console.log('Found ' + featureCollection.features.length + ' features');
-              // alert(featureCollection.features[i]..);
-              rink_name = featureCollection.features[i].properties.rink_name; //place holders for readings layer
-              //reading = featureCollection.features[i].properties.rink_desc; // put all data for the chart in the popup box
-              //revise with content we want to show in the side bar - images? 
-              $("#feature-list tbody").append('<tr class="feature-row" id="' + i  + '"> <td style="vertical-align: middle;">'+ rink_name +'</td><td class="feature-name">' +  '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-            }
-          }
-        });
-   //   if (map.getBounds().contains(rinks_layer.getLatLng())) {
-   //     $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(rinks_layer) + '" lat="' + rinks_layer.getLatLng().lat + '" lng="' + rinks_layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/theater.png"></td><td class="feature-name">' +  '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-      }
+// function syncSidebar() {
+//  Empty sidebar features 
+// $("#feature-list tbody").empty();
+// /* Loop through rinks layer and add only features which are in the map bounds */
+//   if (map.hasLayer(rinksLayer)) {
+//       var Q = L.esri.query({url: rinks_url}); //change to readings_url once it is working 
+//       Q.within(map.getBounds());
+//       Q.run(function(error, featureCollection){
+//         //alert(featureCollection);
+//         if(featureCollection) {  
+//           for (i = 0; i < featureCollection.features.length; i++) { 
+//             //console.log('Found ' + featureCollection.features.length + ' features');
+//             // alert(featureCollection.features[i]..);
+//             rink_name = featureCollection.features[i].properties.rink_name; //place holders for readings layer
+//             //reading = featureCollection.features[i].properties.rink_desc; // put all data for the chart in the popup box
+//             //revise with content we want to show in the side bar - images? 
+//             $("#feature-list tbody").append('<tr class="feature-row" id="' + i  + '"> <td style="vertical-align: middle;">'+ rink_name +'</td><td class="feature-name">' +  '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+//           }
+//         }
+//       });
+//  //   if (map.getBounds().contains(rinks_layer.getLatLng())) {
+//  //     $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(rinks_layer) + '" lat="' + rinks_layer.getLatLng().lat + '" lng="' + rinks_layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/theater.png"></td><td class="feature-name">' +  '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+//     }
     
   
   /* Update list.js featureList */
