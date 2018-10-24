241259f6e13f043e8119dc08096142d221599f37
HigherEd <37836599+hlawrence-esrica@users.noreply.github.com>
2018-10-24 11:39:12 -0400

    d

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ var skateableLayer = new L.featureGroup();
 var notskateableLayer = new L.featureGroup();
 
 // links to the two Survey123 hosted feature services
-var rinks_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/service_29b1d3930876401ab63286e490c601f6/FeatureServer/0';
+var rinks_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/survey123_0a3a8fbc198847a3bd5f6dc645c4dcd7_stakeholder/FeatureServer/0';
 var readings_url = 'https://services1.arcgis.com/OAsihu89uae6w8NX/arcgis/rest/services/service_f8d1476ec5df4d349f953bd14a880be8/FeatureServer/0';
 var show_skateable_how_many_days_ago = 7;
 
